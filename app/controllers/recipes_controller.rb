262bd026184ff503c09b7ab2dc30240a7b745d4b
Justin Vanderheide <justin@justinvanderheide.com>
2012-08-12 14:20:36 -0400

    Sort most to least profitable, and filter by crafting level

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   caches_page :index
   
   def index
-    @recipes = Recipe.valid
+    @recipes = Recipe.valid.sort_by{|r| -r.profit_margin}
     @professions = Profession.all
   end
 end
