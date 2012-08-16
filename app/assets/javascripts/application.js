e3cd699328dc73416e867b255e8b1be9c7089946
Justin Vanderheide <justin@justinvanderheide.com>
2012-08-16 15:41:53 -0400

    Templating

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,37 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+var updateRecipes = function(profession, level) {
+      recipes = $('div[data-profession="' + profession + '"]');
+      recipes.each(function() {
+        var self = $(this);
+        if (self.data('level') > level) {
+          self.hide();
+        } else {
+          self.show();
+        }
+      });
+    };
+  $(function() {
+    
+
+    $('input').change(function(input) {
+      var self = $(this)[0];
+      profession = self.name;
+      level = self.value;
+      localStorage.setItem(profession + "_level", level);
+      updateRecipes(profession, level);
+    });
+
+    $('input').each(function() {
+      var self = $(this);
+      var profession = self.attr('name');
+      var level = localStorage.getItem(profession + "_level");
+      if (level == null) {
+        level = 0;
+      }
+      self.attr('value', level);
+      updateRecipes(profession, level);
+    });
+  });
