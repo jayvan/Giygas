66ff25eaf0914b5a913b6d5e30c85b6edb7695a8
Justin Vanderheide <justin@justinvanderheide.com>
2012-08-19 11:06:16 -0400

    Count up the 'last updated' time

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,36 +14,69 @@
 //= require jquery_ujs
 //= require_tree .
 
-var updateRecipes = function(profession, level) {
-      recipes = $('div[data-profession="' + profession + '"]');
-      recipes.each(function() {
-        var self = $(this);
-        if (self.data('level') > level) {
-          self.hide();
-        } else {
-          self.show();
-        }
-      });
-    };
-  $(function() {
-    
-
-    $('input').change(function(input) {
-      var self = $(this)[0];
-      profession = self.name;
-      level = self.value;
-      localStorage.setItem(profession + "_level", level);
-      updateRecipes(profession, level);
-    });
-
-    $('input').each(function() {
+$(function() {
+  var updateRecipes = function(profession, level) {
+    recipes = $('div[data-profession="' + profession + '"]');
+    recipes.each(function() {
       var self = $(this);
-      var profession = self.attr('name');
-      var level = localStorage.getItem(profession + "_level");
-      if (level == null) {
-        level = 0;
+      if (self.data('level') > level) {
+        self.hide();
+      } else {
+        self.show();
       }
-      self.attr('value', level);
-      updateRecipes(profession, level);
     });
+  };
+
+  var updateCounter = function() {
+    var rollover = updateTime('seconds', 60);
+    if (rollover) {
+      rollover = updateTime('minutes', 60);
+      if (rollover) {
+        updateTime('hours', 0);
+      }
+    }
+  };
+
+  // timeUnit: One of: hours, minutes, or seconds
+  // rollover: the value at which the unit of time rolls over, or 0 for no rollover
+  // Returns: true if a rollover occured, false otherwise
+  var updateTime = function(timeUnit, rollover) {
+    rollover = typeof rollover !== 'undefined' ? rollover : 0;
+
+    var span = $('#' + timeUnit + '_since_update').show();
+    var timeValue = parseInt(span.children('.value').text(), 10) + 1;
+    var newValue = rollover === 0 ? timeValue : timeValue % rollover;
+
+    span.children('.value').text(newValue);
+
+    if (newValue === 1) {
+      span.children('.plural').hide();
+    } else {
+      span.children('.plural').show();
+    }
+
+    return newValue === 0;
+  };
+
+  // Count up the 'updated_at' counter
+  setInterval(function() { updateCounter(); }, 1000);
+
+  $('input').change(function(input) {
+    var self = $(this)[0];
+    profession = self.name;
+    level = self.value;
+    localStorage.setItem(profession + "_level", level);
+    updateRecipes(profession, level);
   });
+
+  $('input').each(function() {
+    var self = $(this);
+    var profession = self.attr('name');
+    var level = localStorage.getItem(profession + "_level");
+    if (level === null) {
+      level = 0;
+    }
+    self.attr('value', level);
+    updateRecipes(profession, level);
+  });
+});
